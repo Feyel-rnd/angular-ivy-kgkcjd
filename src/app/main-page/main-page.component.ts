19bd7285dc5cb64af867d6593241e713b0b58730
Feyel-rnd <113343892+Feyel-rnd@users.noreply.github.com>
2022-10-13 12:15:38 +0200

    3 files modified

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -25,7 +25,7 @@ export class MainPageComponent implements OnInit {
    email : string;
    result : any;
   // app : any
-  isOut =  false;
+  isOut =  true;
   authorized : boolean;
   connected_users : number;
   app = environment.application
